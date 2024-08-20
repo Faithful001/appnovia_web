3772c8d866aecac02a55245e8fbfdf0890b819a3
faithfultheking <83516086+Faithful001@users.noreply.github.com>
2024-08-20 12:01:48 +0100

    resolved the sofware error

--- components/hero/HeroTwo.tsx
+++ components/hero/HeroTwo.tsx
@@ -2,9 +2,9 @@ import Button from "../ui/button/Button";
 
 const HeroTwo = () => {
   return (
-    <div className="lg:w-1/2 md:w-2/3 flex flex-col gap-10 z-10 transition-all duration-300">
-      <div className="flex flex-col gap-3">
-        <h1 className="font-semibold md:text-5xl sm:text-[2.55rem] text-4xl md:leading-[72px] sm:leading-[70px]">
+    <div className="lg:w-1/2 md:w-2/3 flex flex-col gap-7 z-10 transition-all duration-300">
+      <div className="flex flex-col gap-1">
+        <h1 className="font-semibold md:text-5xl sm:text-[2.55rem] text-4xl md:leading-[63px] sm:leading-[70px]">
           Innovative <span className="text-brandColor">Services</span> for Every
           Industry
         </h1>
