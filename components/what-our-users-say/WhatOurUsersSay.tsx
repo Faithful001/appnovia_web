13244ca1e36719bb3ab47b9ef3cf850d482401df
faithfultheking <83516086+Faithful001@users.noreply.github.com>
2024-08-20 11:24:35 +0100

    resolved the build errors

--- components/what-our-users-say/WhatOurUsersSay.tsx
+++ components/what-our-users-say/WhatOurUsersSay.tsx
@@ -26,7 +26,7 @@ const WhatOurUsersSay = () => {
           What Our Users Say About Us
         </h1>
         <p className="tracking-wide text-base text-center">
-          These testimonials showcase Deximpli's dedication to driving
+          These testimonials showcase Deximplis dedication to driving
         </p>
       </span>
       <div
